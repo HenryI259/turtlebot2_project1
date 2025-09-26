cb9db49af0fd33bafcd0bbd9512ba4d162907686
Henry <hison259@gmail.com>
2025-09-26 12:11:23 -0500

    setup all of the behaviors. TODO implement behaviors

--- src/move.cpp
+++ src/move.cpp
@@ -2,6 +2,9 @@
 #include "geometry_msgs/Twist.h"
 #include "nav_msgs/Odometry.h"
 #include <random>
+#include <thread>
+
+using namespace std;
 
 double pos_x;
 double pos_y;
@@ -11,13 +14,13 @@ void getPosition(const nav_msgs::Odometry::ConstPtr& msg) {
     pos_y = msg->pose.pose.position.y;
 }
 
-struct moter_wire_t {
+struct motor_wire_t {
     int suppression;
     int linear_value;
     int angular_value;
 };
 
-struct moter_wire_t drive_wire;
+struct motor_wire_t drive_wire;
 
 void drive_randomly(ros::Publisher pub, ros::Rate rate) {
     double linear_wire;
@@ -58,6 +61,47 @@ void drive_randomly(ros::Publisher pub, ros::Rate rate) {
     
 }
 
+struct motor_wire_t avoid_wire;
+
+// TO-DO make the robot avoid asymetric and symetric obstacles
+void avoid_obstacles() {
+
+    while(ros::ok()) {
+
+    }
+
+    if (avoid_wire.suppression) {
+        drive_wire.suppression = true;
+        drive_wire.linear_value = avoid_wire.linear_value;
+        drive_wire.angular_value = avoid_wire.angular_value;
+    }
+}
+
+struct motor_wire_t keyboard_wire;
+
+// TO-DO send human input to the robot
+void accept_keyboard_movement() {
+
+    while(ros::ok()) {
+        
+    }
+
+    if (keyboard_wire.suppression) {
+        avoid_wire.suppression = true;
+        avoid_wire.linear_value = keyboard_wire.linear_value;
+        avoid_wire.angular_value = keyboard_wire.angular_value;
+    }
+}
+
+// TO-DO make the robot halt if there is a collision
+void halt() {
+    
+    while(ros::ok()) {
+        
+    }
+
+}
+
 
 int main(int argc, char **argv)
 {
@@ -68,10 +112,17 @@ int main(int argc, char **argv)
     ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 10);
     ros::Subscriber odom_sub = nh.subscribe("/odom", 10, getPosition);
 
+    ros::Rate rate(10);
 
-    ros::Rate rate(10); // 10 Hz
+    thread t1(drive_randomly, pub, rate);
+    thread t2(avoid_obstacles);
+    thread t3(accept_keyboard_movement);
+    thread t4(halt);
 
-    drive_randomly(pub, rate);
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
 
     return 0;
 }
